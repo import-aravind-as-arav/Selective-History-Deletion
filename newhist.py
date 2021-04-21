da2dc7ee97fe5d8783c8d284ac1d2c207ef235a3
Aravind A <55825898+import-aravind-as-arav@users.noreply.github.com>
2021-04-21 17:11:31 +0530

    Add files via upload

---
+++ newhist.py
@@ -0,0 +1,18 @@
+import sqlite3
+
+
+conn=sqlite3.connect('C:/Users/arav_/AppData/Local/BraveSoftware/Brave-Browser/User Data/Default/History')
+c=conn.cursor()
+
+web=str(input("Enter the website: "))
+query="SELECT id,url FROM urls WHERE url like '%"+web+"%'"
+
+id_select=[]
+for rows in c.execute(query):
+    print("Deleting....",rows[1])
+    id_del=rows[0]
+    id_select.append((id_del,))
+c.executemany('DELETE from urls WHERE id=?',id_select)
+conn.commit()
+
+conn.close()
